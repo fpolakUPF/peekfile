4f3a1984d1f1f52da30f3fef07f1579a9ebd2e68
Frank Polak <frankpolak@Franks-MacBook-Pro.local>
2023-11-02 11:40:24 +0100

    homework 1

--- peekfile.sh
+++ peekfile.sh
@@ -1,4 +1,17 @@
 #!/bin/zsh
-head -n $2 $1
-echo ...
-Tail -n $2 $1
+FILE=$1
+L_COUNT=$2
+if [ -z $L_COUNT ]
+then
+	L_COUNT=3
+fi
+if [ $(wc -l < $FILE) -le $((2 * $L_COUNT)) ]
+then
+	echo "Number of lines in file smaller than requested"
+	cat $FILE
+else
+	echo "displaying requested lines"
+	head -n $L_COUNT $FILE
+	echo ...
+	tail -n $L_COUNT $FILE
+fi
