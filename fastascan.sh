685717a4118c16f377c965196de3459fcdc1a6e1
Frank Polak <frankpolak@Franks-MacBook-Pro.local>
2023-11-22 21:14:12 +0100

    minor corrections

--- fastascan.sh
+++ fastascan.sh
@@ -4,7 +4,7 @@ N=$2
 
 if [[ -z $FOLDER ]]
 then
-    FOLDER=$(pwd)
+    FOLDER="./"
 fi 
 
 if [[ -z $N ]]
@@ -16,14 +16,18 @@ fasta_files=$(find . "$FOLDER" -type f -name "*.fasta" -or -type f -name "*fa")
 unique_ids=""
 
 echo "\n----FASTAscan Report----"
-echo "Folder selected:\t\t" $FOLDER 
+if [[ $FOLDER == "./" ]]; then
+    echo "Folder selected:\t\t" $(pwd)
+else
+    echo "Folder selected:\t\t" $FOLDER
+fi
 echo "Number of files:\t\t" $(echo $fasta_files | wc -l)
 
 for file in $(echo $fasta_files); do
     unique_ids+=$(grep ">" $file | awk '{print $1}' | sort | uniq -c)
 done
 
-echo "Total umber of unique IDs:\t" $(echo $unique_ids | sort | uniq -c | wc -l)
+echo "Total umber of unique IDs:\t" $(echo $unique_ids | sort | uniq -c | wc -l) "\n"
 
 
 for file in $(echo $fasta_files); do
@@ -33,14 +37,15 @@ for file in $(echo $fasta_files); do
 	else
 		echo "(NOT A SYMBOLIC LINK)"
 	fi
-    echo "Number of sequences:" $(grep ">" $file | wc -l)
+
+    echo "Number of sequences:\t" $(grep ">" $file | wc -l)
     seqs_length=0
     while read -r line; do
         if [[ $line != ">*" ]]; then
-            seqs_length=$(( $seqs_length + $(echo $line | awk '/[^A-Za-z]/ { gsub(/[^A-Z]/, ""); print }' | wc -c) ))
+            seqs_length=$(( $seqs_length + $(echo $line | awk '{ gsub(/[^A-Za-z]/, ""); print }' | wc -c) ))
         fi
     done < "$file"
-    echo "Total sequence length: " $seqs_length
+    echo "Total sequence length:\t" $seqs_length
     
     if [[ $N -gt 0 ]]; then
         if [[ $(cat $file | wc -l) -lt  $(( 2 * $N )) ]]; then
@@ -54,5 +59,4 @@ for file in $(echo $fasta_files); do
     fi
 done
 
-
-
+echo "----END----"
